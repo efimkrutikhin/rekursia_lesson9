6ba6f458fe2ce1da6d5c1b1ec4750804989d645d
efim <efim.krutihin@yandex.ru>
2023-06-21 17:16:27 +0300

    completed task1

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = 5;
+        Console.WriteLine("Натуральные числа от {0} до 1:", N);
+        PrintNaturalNumbers(N);
+    }
+
+    static void PrintNaturalNumbers(int num)
+    {
+        if (num > 0)
+        {
+            Console.Write(num + " ");
+            PrintNaturalNumbers(num - 1);
+        }
+    }
+}
