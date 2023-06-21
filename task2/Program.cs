ece7832c58950cbed941cba37acf0e86ffd802f3
efim <efim.krutihin@yandex.ru>
2023-06-21 17:17:39 +0300

    completed task2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int M = 1;
+        int N = 15;
+        int sum = CalculateSum(M, N);
+        Console.WriteLine("Сумма натуральных элементов от {0} до {1}: {2}", M, N, sum);
+    }
+
+    static int CalculateSum(int start, int end)
+    {
+        if (start > end)
+            return 0;
+        
+        return start + CalculateSum(start + 1, end);
+    }
+}
